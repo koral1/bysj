9013d7dfcb9bb814cb5d6a968c90b6d9b2384970
koral1 <760173603@qq.com>
2022-04-22 20:38:51 +0800

    测试flask

---
+++ test.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from wsgiref.simple_server import make_server 
+ 
+app=Flask(__name__) 
+ 
+@app.route('/')      #路由系统生成 视图对应url,1. decorator=app.route() 2. decorator(first_flask)
+def first_flask():    #视图函数
+    return 'Hello Worldcaicai'  #response
+ 
+if __name__ == '__main__':
+    server = make_server('', 5000, app)
+    server.serve_forever()#启动socket
+
